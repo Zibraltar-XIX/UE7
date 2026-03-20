0f4c41aa16b2129c5b15ef879f33175c9e802525
Parzival <jparola@guardiaschool.fr>
2026-03-20 10:51:29 +0100

    Update publish.bat

--- publish.bat
+++ publish.bat
@@ -10,9 +10,7 @@ if "%VERSION%"=="" (
 
 set IMAGE=parzivalxix/alternance-tah-les-fous
 
-docker buildx create --use --name multiarch-builder
-docker buildx inspect --bootstrap
-
+docker buildx use multiarch-builder
 docker buildx build --platform linux/amd64,linux/arm64 -f .\app\Dockerfile -t %IMAGE%:%VERSION% -t %IMAGE%:latest --push .
 
 pause
